32e024dca9f50dbe4c332ea6b1fbb0f501b3be89
yasas20050110 <yasassanjaya9@gmail.com>
2025-11-10 12:04:59 +0530

    Project-Initial_stpe-week01

---
+++ laptopkade/src/main/java/com/soul/laptopkade/LaptopkadeApplication.java
@@ -0,0 +1,13 @@
+package com.soul.laptopkade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopkadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopkadeApplication.class, args);
+	}
+
+}
---
+++ laptopkade/src/test/java/com/soul/laptopkade/LaptopkadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soul.laptopkade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopkadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
